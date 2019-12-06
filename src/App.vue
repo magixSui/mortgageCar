e3fe39635d52dc66a3a5182707bbc3c8aac34f31
MagixSui <suixin@jcinfo.online>
2019-12-06 13:52:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
     <van-tabbar-item icon="exchange">代理</van-tabbar-item>
     <van-tabbar-item icon="friends-o">我的</van-tabbar-item>
   </van-tabbar>
-  </div> 
+  </div>
 </template>
 
 <script>
